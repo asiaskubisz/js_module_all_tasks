818cd19f614aa1789d2b1d95b719f4eec7af9911
asiaskubisz <joannaskubisz85@gmail.com>
2017-03-03 22:28:10 +0100

    added all files

---
+++ README.md
@@ -0,0 +1,2 @@
+# javamoduletask1
+First task from module on java script
---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// plik scripts.js
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(15, 25);
+var triangle3Area = getTriangleArea(40, 65);
+
+function getTriangleArea(a, h) {
+    if (a <= 0 || h <= 0){
+        console.log('Nieprawidłowe dane');
+}       
+    return (a*h/4)};
+
+console.log( getTriangleArea(10,6) )
+console.log(triangle1Area)
+console.log(triangle2Area)
+console.log(triangle3Area)
+// dodatkowe zmiany
